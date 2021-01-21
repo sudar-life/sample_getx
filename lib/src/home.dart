b9ec93442fd60726d14023a3479fd6b74bc3036e
sudar-life <mousai86@gmail.com>
2021-01-21 21:30:08 +0900

    getX reactiveState increase event

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+  Home({Key key}) : super(key: key);
+
+  var count = 0.abs();
+
+  void increase() {
+    count++;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ReactiveState"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Obx(
+              () => Text(
+                count.toString(),
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            RaisedButton(
+              child: Text("더하기"),
+              onPressed: increase,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
