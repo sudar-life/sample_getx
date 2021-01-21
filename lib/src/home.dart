f09b7c4705f603babbd4ddb855180d1bbb51639b
sudar-life <mousai86@gmail.com>
2021-01-21 22:17:19 +0900

    state management with class

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:sample_getx/src/user.dart';
+import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+  Home({Key key}) : super(key: key);
+
+  Rx<User> user = User(name: "개남", email: "mousai86@gmail.com", age: 22).obs;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rx State Management with Class"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(() {
+              return Text(
+                "${user.value.name} : ${user.value.age} \n ${user.value.email}",
+              );
+            }),
+            RaisedButton(
+              child: Text("change name"),
+              onPressed: () {
+                user.update((u) {
+                  u.name = "개발하는남자";
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/user.dart
@@ -0,0 +1,6 @@
+class User {
+  String name;
+  String email;
+  int age;
+  User({this.name, this.email, this.age});
+}
