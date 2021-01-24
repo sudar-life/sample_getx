0b08fdc28367ea0dfd934d8993f3a9afd662adca
sudar-life <mousai86@gmail.com>
2021-01-24 19:05:39 +0900

    feat : getView  - update 방식과 reactive 방식으로 getview 사용하기

---
+++ lib/src/controller/count_controller_with_reactive.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class CountControllerWithReactive extends GetxController {
+  RxInt count = 0.obs;
+  void increase() {
+    count++;
+  }
+}
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample_getx/src/pages/get_view_widget.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GET X"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text("Get View"),
+              onPressed: () {
+                Get.to(GetViewWidget());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
